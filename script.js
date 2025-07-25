b98ec97b42efa05be0d5ffa5a94d7ace37e16de3
sachingiri58 <sachinggiri01@gmail.com>
2025-07-25 19:15:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
-const menuOpenButton=document.querySelector("#menu-open-button");
-menuOpenButton.addEventListener("click",() =>{
-  document.body.classList.toggle("show-mobile-menu");
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+
+menuOpenButton.addEventListener("click", () => {
+  document.body.classList.add("show-mobile-menu");
+});
+
+menuCloseButton.addEventListener("click", () => {
+  document.body.classList.remove("show-mobile-menu");
+});
+
+const swiper = new Swiper(".slider-container", {
+  loop: true,
+  grabCursor: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
 });
